9a51a7f964bdce8ce2389ab4d652c85d93094695
Thomas RIchards <thomas.c.richards1@gmail.com>
2020-01-02 20:44:11 +0100

    created player controller, added ability to move character using rigidbody2d. Craeted animations out of sprite sheet for walking.

--- The Legend of Lunk/Assets/Scripts/PlayerController.cs
+++ The Legend of Lunk/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Vector2 mInput;
+    private Rigidbody2D mRigidbody;
+    Animator mAnimator;
+
+    private void Start()
+    {
+        mRigidbody = GetComponent<Rigidbody2D>();
+        mAnimator = GetComponent<Animator>();
+        if (mRigidbody == null)
+        {
+            mRigidbody = gameObject.AddComponent<Rigidbody2D>();
+        }
+    }
+    
+    private void FixedUpdate()
+    {
+        GetInput();
+        Move();
+        UpdateAnimation();
+    }
+    private void Update()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    private void Move()
     {
-        
+        Vector2 newPosition = Vector2.zero;
+        float speedModifier = 0.15f;
+        newPosition.x = speedModifier * mInput.x + transform.position.x;
+        newPosition.y = speedModifier * mInput.y + transform.position.y;
+        mRigidbody.MovePosition(newPosition);
+    }
+    private void GetInput()
+    {
+        mInput = Vector2.zero;
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            mInput.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            mInput.y += 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            mInput.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            mInput.x += 1;
+        }
+    }
+    void UpdateAnimation()
+    {
+        mAnimator.SetFloat("MoveX", mInput.x);
+        mAnimator.SetFloat("MoveY", mInput.y);
     }
 }
